d72d7f5a37ad47657310445aedbc28d870ee65f2
Bootcamp <Bootcamp@PICCPU-0001>
2016-10-14 17:49:50 +0800

    Added Password Manager.

---
+++ PastebookBusinessLogicLibrary/PasswordManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastebookBusinessLogicLibrary
+{
+    public class PasswordManager
+    {
+        private static RNGCryptoServiceProvider m_cryptoServiceProvider = null;
+        private const int SALT_SIZE = 24;
+
+        public string GetPasswordHashAndSalt(string message)
+        {
+            // Let us use SHA256 algorithm to 
+            // generate the hash from this salted password
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] dataBytes = Utility.GetBytes(message);
+            byte[] resultBytes = sha.ComputeHash(dataBytes);
+
+            // return the hash string to the caller
+            return Utility.GetString(resultBytes);
+        }
+
+        public string GeneratePasswordHash(string plainTextPassword, out string salt)
+        {
+            salt = SaltGenerator.GetSaltString();
+
+            string finalString = plainTextPassword + salt;
+
+            return m_hashComputer.GetPasswordHashAndSalt(finalString);
+        }
+
+        public bool IsPasswordMatch(string password, string salt, string hash)
+        {
+            string finalString = password + salt;
+            return hash == m_hashComputer.GetPasswordHashAndSalt(finalString);
+        }
+
+        static SaltGenerator()
+        {
+            m_cryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+
+        public string GetSaltString()
+        {
+            // Lets create a byte array to store the salt bytes
+            byte[] saltBytes = new byte[SALT_SIZE];
+
+            // lets generate the salt in the byte array
+            m_cryptoServiceProvider.GetNonZeroBytes(saltBytes);
+
+            // Let us get some string representation for this salt
+            string saltString = Utility.GetString(saltBytes);
+
+            // Now we have our salt string ready lets return it to the caller
+            return saltString;
+        }
+
+    }
+}
